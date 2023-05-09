564a7da5e391a43e04040d937e27ae11c17b4ed1
jbourg4364 <jasonjbourg22@gmail.com>
2023-05-09 13:15:19 -0500

    added all react files

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Main from './components/Main';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-        <Main />
+        <BrowserRouter>
+                <Main />
+        </BrowserRouter>
+        
 );
