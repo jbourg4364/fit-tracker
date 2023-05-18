9b4258a235d5c8fc581509ba4767d71c5f086cad
jbourg4364 <jasonjbourg22@gmail.com>
2023-05-18 12:29:56 -0500

    init

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import { registerUser } from '../api-client/auth';
 import './Register.css'
 const Register = () => {
